b1ca0125e30549f928b949ec37bcc69d9ea15bc8
brianerikson <briankerikson@gmail.com>
2016-07-11 22:04:07 +0200

    added temp notification retrieval test

--- src/main.rs
+++ src/main.rs
@@ -5,10 +5,15 @@ extern crate rustc_serialize;
 extern crate env_logger;
 mod configuration;
 //use notify_rust::{Notification, NotificationHint, NotificationUrgency};
+use rusthub::notifications;
+use rusthub::notifications::NotificationResponse;
 
 fn main() {
     const TIMEOUT: i32 = 120;
     env_logger::init().unwrap();
     let token: String = configuration::retrieve_token(TIMEOUT);
     debug!("Token: {}", token);
+    //
+    let response: NotificationResponse = notifications::get_notifications_oauth(token).unwrap();
+    debug!("Notification: {:#?}", response);
 }
