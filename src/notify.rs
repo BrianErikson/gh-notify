7e13e6e149c1217200bc5551056a96f1485a46a9
brianerikson <briankerikson@gmail.com>
2016-07-13 07:58:57 +0200

    notifications are now more legible

--- src/notify.rs
+++ src/notify.rs
@@ -1,25 +1,31 @@
 use notify_rust::{Notification, NotificationHint, NotificationUrgency};
 use rusthub::notifications;
 use std::process::Command;
+use std::thread;
 
 const APP_NAME: &'static str = "gh-notify";
 
 pub fn show_notification(notification: &notifications::Notification) {
-    notify_action(
-        &notification.subject.title,
-        &notification.reason,
-        "Open in Browser",
-        120,
-        |action| {
-            match action {
-                "default" | "clicked" => {
-                    open_link(&notification.url);
-                },
-                "__closed" => (),
-                _ => ()
+    let subject = format!("{} - {}", notification.repository.name, notification.subject.subject_type);
+    let body = notification.subject.title.clone();
+    let url = notification.repository.html_url.clone();
+    thread::spawn(move || {
+        notify_action(
+            &subject,
+            &body,
+            "Open in Browser",
+            120,
+            |action| {
+                match action {
+                    "default" | "clicked" => {
+                        open_link(&url);
+                    },
+                    "__closed" => (),
+                    _ => ()
+                }
             }
-        }
-    );
+        );
+    });
 }
 
 pub fn open_link(url: &str) {
